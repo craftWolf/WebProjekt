7499015a4ed9cc7a472d018dd35dde1369c68bf4
craftWolf <mh.chirosca@gmail.com>
2018-12-16 00:15:17 +0100

    text

--- BattleShip2/app.js
+++ BattleShip2/app.js
@@ -10,7 +10,6 @@ var port = process.argv[2];
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
 
 app.get("/",function(req,res){
   res.sendFile("splash.html",{root: "./public"});
--- BattleShip2/game.js
+++ BattleShip2/game.js
@@ -8,25 +8,6 @@ var game = function (gameID) {
     this.gameState = "0 JOINT"; //"A" means A won, "B" means B won, "ABORTED" means the game was aborted
 };
 
-/*
- * The game can be in a number of different states.
- */
-game.prototype.transitionStates = {};
-game.prototype.transitionStates["0 JOINT"] = 0;
-game.prototype.transitionStates["1 JOINT"] = 1;
-game.prototype.transitionStates["2 JOINT"] = 2;
-game.prototype.transitionStates["A HIT"] = 3;
-game.prototype.transitionStates["B HIT"] = 4;
-game.prototype.transitionStates["A MISSED"] = 5;
-game.prototype.transitionStates["B MISSED"] = 6;
-game.prototype.transitionStates["A"] = 7; //A won
-game.prototype.transitionStates["B"] = 8; //B won
-game.prototype.transitionStates["ABORTED"] = 9;
-
-
-
-
-
 
 
 module.exports = game;
