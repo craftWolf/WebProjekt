cc10f4092655b0df936a53f9c4656ff639c9957a
craftWolf <mh.chirosca@gmail.com>
2018-12-15 22:44:04 +0100

    text

--- BattleShip2/app.js
+++ BattleShip2/app.js
@@ -2,44 +2,12 @@ var express = require("express");
 var http = require("http");
 var websocket = require("ws")
 
-var indexRouter = require("./routes/index");
-// var messages = require("./public/javascripts/messages");
-
-var gameStatus = require("./statTracker");
-var Game = require("./game");
-
 var port = process.argv[2];
 var app = express();
-
 
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
 
 app.get("/",function(req,res){
   res.sendFile("splash.html",{root: "./public"});
-})
-
-var server = http.createServer(app);
-const wss = new websocket.Server({ server });
-
-var websockets = {};//property: websocket, value: game
-
-var currentGame = new Game(gameStatus.gamesInitialized++);
-var connectionID = 0;//each websocket receives a unique ID
-
-wss.on("connection", function connection(ws) {
-
-  /*
-   * two-player game: every two players are added to the same game
-   */
-  let con = ws;
-  con.id = connectionID++;
-  let playerType = currentGame.addPlayer(con);
-  websockets[con.id] = currentGame;
-
-  /*
-   * inform the client about its assigned player type
-   */
-  // con.send((playerType == "A") ? messages.S_PLAYER_A : messages.S_PLAYER_B);
-
 })
